8cbec686f4b2c7c9bacb503844086ccf4c8b8670
thint2908 <thint2908@gmail.com>
2026-04-15 14:34:23 +0700

    [fix] Change due_date field type to Datetime and update overdue badge

--- custom-module/todo_app/models/todo_task.py
+++ custom-module/todo_app/models/todo_task.py
@@ -21,7 +21,8 @@ class TodoTask(models.Model):
         default=lambda self: fields.Datetime.now(),
         readonly=True
     )
-    due_date = fields.Date(string='Due Date', tracking=True)
+    due_date = fields.Datetime(string='Due Date', tracking=True)
+    # due_date = fields.Date(string='Due Date', tracking=True)
     is_overdue = fields.Boolean(compute='_compute_is_overdue', string='Is Overdue')
     remaining_time = fields.Char(compute="_compute_remaining_time", string="Remaining")
     priority = fields.Selection([
@@ -43,34 +44,33 @@ class TodoTask(models.Model):
 
     @api.depends('due_date', 'state')
     def _compute_is_overdue(self):
-        today = date.today()
+        now = fields.Datetime.now()
+        # today = date.today()
         for record in self:
-            if record.due_date and record.due_date < today and record.state != 'done':
+            if record.due_date and record.due_date < now:
                 record.is_overdue = True
             else:
                 record.is_overdue = False
-    # @api.depends('due_date', 'is_done')
-    # def _compute_is_overdue(self):
-    #     today = date.today()
-    #     for record in self:
-    #         if record.due_date and record.due_date < today and not record.is_done:
-    #             record.is_overdue = True
-    #         else:
-    #             record.is_overdue = False
                 
     @api.depends("due_date", "state")
     def _compute_remaining_time(self):
-        today = date.today()
+        now = fields.Datetime.now()
         for record in self:
             if record.state == 'done':
                 record.remaining_time = "Completed"
             elif not record.due_date:
                 record.remaining_time = "No Deadline"
             else:
-                diff = (record.due_date - today).days
-                if diff > 0:
-                    record.remaining_time = f"{diff} days"
-                if diff == 0:
-                    record.remaining_time = "Today!"
+                # Tính toán khoảng cách thời gian
+                diff = record.due_date - now
+                days = diff.days
+                seconds = diff.seconds
+                hours = seconds // 3600
+
+                if diff.total_seconds() > 0:
+                    if days > 0:
+                        record.remaining_time = f"{days}d {hours}h left"
+                    else:
+                        record.remaining_time = f"{hours}h left"
                 else:
-                    record.remaining_time = f"({diff} days)"
+                    record.remaining_time = "Overdue!"
