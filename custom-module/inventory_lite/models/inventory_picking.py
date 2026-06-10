4d79076f69c08c87ead6ddaf18c61ae4b2fddd39
thint2908 <thint2908@gmail.com>
2026-06-10 22:48:46 +0700

    feat: add move & picking model

---
+++ custom-module/inventory_lite/models/inventory_picking.py
@@ -0,0 +1,25 @@
+from odoo import fields, models
+
+
+class InventoryPicking(models.Model):
+    _name="inventory.picking"
+    _description="Inventor Picking"
+
+    name = fields.Char(string="Ref Code", required=True, default="New")
+    picking_type = fields.Selection([
+        ('incoming', "Incoming"),
+        ('outgoing', "Out Going"),
+        ('internal', "Internal")
+    ], string="Operation Type", required=True)
+
+    src_location_id = fields.Many2one('inventory.location', string="From location", required=True)
+    dest_location_id = fields.Many2one('inventory.location', string="To location", required=True)
+
+    move_ids = fields.One2many('inventory.move','picking_id', string="Stock moves")
+    
+    state = fields.Selection([
+            ('draft', 'Draft'),
+            ('confirmed', 'Confirmed'),
+            ('done', 'Done'),
+            ('cancelled', 'Cancelled'),
+        ], string="State", default='draft', required=True)
