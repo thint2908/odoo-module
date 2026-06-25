d4266541ba06ff05f17f5f3b18fb836b87c3439a
thint2908 <thint2908@gmail.com>
2026-06-25 11:00:28 +0700

    feat: add qty_available field to inventory product views and update computation logic

--- custom-module/inventory_lite/models/inventory_product.py
+++ custom-module/inventory_lite/models/inventory_product.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class InventoryProduct(models.Model):
@@ -10,6 +10,21 @@ class InventoryProduct(models.Model):
     active = fields.Boolean(default=True)
     cost_price = fields.Float(string='Cost price')
     qty_available = fields.Float(string="Available Quantity", compute="_compute_qty_available")
+    quant_ids = fields.One2many("inventory.quant", "product_id", string="Quants")
     
-    
-    
+    @api.depends("quant_ids.quantity", "quant_ids.location_id.usage")
+    def _compute_qty_available(self):
+        for product in self:
+            # quants = self.env["inventory.quant"].search([
+            #     ("product_id", "=", product.id),
+            #     ("location_id.usage", "=", "internal")
+            # ])
+            
+            # product.qty_available = sum(quants.mapped("quantity"), 0.0)
+            internal_quants = product.quant_ids.filtered(
+                lambda q: q.location_id.usage == "internal"
+            )
+            
+            product.qty_available = sum(
+                internal_quants.mapped("quantity")
+            )
